3160582eef654c8d1e6516b8a07ab6467ff21e4e
tnosekk <nosektomek@gmail.com>
2025-05-14 21:32:03 +0200

    added UI, working on making update,add,remove function work.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -14,7 +15,7 @@ import (
 )
 
 type Todo struct {
-	ID        bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	ID        bson.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Completed bool          `json:"completed"`
 	Body      string        `json:"body"`
 }
@@ -49,6 +50,11 @@ func main() {
 	collection = client.Database("golang_db").Collection("todos")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin,Content-Type,Accept",
+	}))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
